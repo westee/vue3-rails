c5400b669e14de4459d78304e610981353c9a3e4
westee <904885508@qq.com>
2020-08-03 21:57:37 +0800

    切换开关状态

--- vite-web/src/components/SwitchDemo.vue
+++ vite-web/src/components/SwitchDemo.vue
@@ -1,3 +1,12 @@
 <template>
-    <div>Switch</div>
+    <div>
+        <Switch></Switch>
+    </div>
 </template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+export default {
+    components: {Switch}
+}
+</script>
---
+++ vite-web/src/lib/Switch.vue
@@ -0,0 +1,53 @@
+<template>
+  <button :class="{checked}" @click="toggleChecked">
+    <span></span>
+  </button>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+export default {
+    setup(){
+        const checked = ref(false);
+
+        const toggleChecked = ()=>{
+            checked.value = !checked.value
+            console.log(checked.value)
+        }
+
+        return { checked, toggleChecked }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+$my-blue: #0092ff;
+$my-grey: #aaa;
+
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: $my-grey;
+  border-radius: $h/2;
+  position: relative;
+
+  span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+  }
+
+  &.checked {
+      background: $my-blue;
+      span {
+    left: calc(100% - #{$h2} - 2px);
+  }}
+}
+</style>
