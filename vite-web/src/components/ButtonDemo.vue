1133720706bf25f074a649191407ef1e29c13187
westee <904885508@qq.com>
2020-08-14 21:07:07 +0800

    创建Button组件

--- vite-web/src/components/ButtonDemo.vue
+++ vite-web/src/components/ButtonDemo.vue
@@ -1,3 +1,17 @@
 <template>
     <div>Button</div>
+    <div>示例1</div>
+    <div>
+        <Button>按钮</Button>
+    </div>
 </template>
+
+<script lang="ts">
+    import Button from '../lib/Button.vue'
+    export default {
+        components:{
+            Button
+        }
+    }
+
+</script>
