ff77ad3352965ef767d80f404cf3e97b47d619c9
westee <904885508@qq.com>
2020-08-20 20:57:01 +0800

    完成 Button 组件

--- vite-web/src/lib/Button.vue
+++ vite-web/src/lib/Button.vue
@@ -1,11 +1,13 @@
 <template>
-    <button class="east-button"
-            :class="`theme-${theme}`">
+    <button class="east-button" :class="classes" :disabled="disabled">
+        <span v-if="loading" class="east-loadingIndicator"></span>
         <slot/>
     </button>
 </template>
 
 <script lang="ts">
+    import {computed} from 'vue'
+
     export default {
         name: "Button",
         inheritAttrs: false,
@@ -13,11 +15,35 @@
             theme: {
                 type: String,
                 default: 'button'
+            },
+            size: {
+                type: String,
+                default: 'normal'
+            },
+            level: {
+                type: String,
+                default: 'normal'
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            loading: {
+                type: Boolean,
+                default: false
             }
         },
         setup(props, context) {
-            const {size, ...rest} = context.attrs
-            return {size, rest}
+            const {theme, size, level, disable} = props;
+            const classes = computed(() => {
+                return {
+                    [`east-theme-${theme}`]: theme,
+                    [`east-size-${size}`]: size,
+                    [`east-size-${level}`]: level,
+
+                }
+            });
+            return {size, classes};
         }
     }
 </script>
