ab3b0025d74caca0c281fd99036d577fed8f300f
westee <904885508@qq.com>
2020-08-28 23:26:30 +0800

    让Dialog支持visible属性

--- vite-web/src/components/DialogDemo.vue
+++ vite-web/src/components/DialogDemo.vue
@@ -1,14 +1,25 @@
 <template>
     <div>Dialog 示例</div>
     <h1>示例1</h1>
-    <Dialog></Dialog>
+    <Button @click="toggle">toggle</Button>
+    <Dialog :visible="x"></Dialog>
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog'
+import Dialog from '../lib/Dialog.vue'
+import {ref} from 'vue'
+import Button from "../lib/Button.vue";
     export default {
         components:{
-            Dialog
+            Dialog,Button
+        },
+        setup(){
+            const x = ref(false);
+            const toggle = () => {
+                console.log(12)
+                x.value = !x.value
+            };
+            return {toggle}
         }
     }
 </script>
