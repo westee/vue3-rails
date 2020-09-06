2e280a29e0aa0c823596a486161f0cc5a74ac775
westee <904885508@qq.com>
2020-09-06 21:39:11 +0800

    创建 Tabs 和 Tab 组件

---
+++ vite-web/src/components/TabsDemo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Tabs>
+            <Tab title="导航1">内容一</Tab>
+            <Tab title="导航2">内容二</Tab>
+        </Tabs>
+    </div>
+</template>
+
+<script lang="ts">
+    import Tab from '../lib/Tab.vue'
+    import Tabs from '../lib/Tabs.vue'
+
+    export default {
+        components:{
+            Tab, Tabs
+        }
+    }
+</script>
