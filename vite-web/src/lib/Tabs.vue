a7c5dffd9a1f392bca706401f821a9865c665d23
westee <904885508@qq.com>
2020-09-07 22:44:17 +0800

    渲染嵌套的插槽

--- vite-web/src/lib/Tabs.vue
+++ vite-web/src/lib/Tabs.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
-        Tabs组件
-        <component :is="defaults[0]"></component>
-        <component :is="defaults[1]"></component>
+    <div class="east-tabs">
+        <div class="east-tabs-nav">
+            <div class="east-tabs-nav-item" v-for="t in titles" :key="t">{{t}}</div>
+        </div>
+        <div class="east-tabs-content">
+            <component v-for="(item, index) in defaults" :is="item" :key="index" class="east-tabs-content-item" />
+        </div>
     </div>
 </template>
 
@@ -10,14 +13,59 @@
     import Tab from '../lib/Tab.vue';
 
     export default {
-        setup(props, context){
+        setup(props, context) {
             const defaults = context.slots.default()
-            defaults.forEach(tag => {{
-                if(tag.type !== Tab){
-                    throw new Error("Tabs的子组件必须为 Tab。")
+            defaults.forEach(tag => {
+                {
+                    if (tag.type.__hmrId !== Tab.__hmrId) {
+                        throw new Error("Tabs的子组件必须为 Tab。")
+                    }
+                    // if (tag.type !== Tab.type) {
+                    //     throw new Error("Tabs的子组件必须为 Tab。")
+                    // }
                 }
-            }})
-            return {defaults}
+            });
+            const titles = defaults.map(tag => {
+                return tag.props.title
+            });
+            return {defaults, titles}
         }
     }
 </script>
+
+<style lang="scss">
+    $blue: #40a9ff;
+    $color: #333;
+    $border-color: #d9d9d9;
+    .east-tabs {
+        &-nav {
+            display: flex;
+            color: $color;
+            border-bottom: 1px solid $border-color;
+            position: relative;
+            &-item {
+                padding: 8px 0;
+                margin: 0 16px;
+                cursor: pointer;
+                &:first-child {
+                    margin-left: 0;
+                }
+                &.selected {
+                    color: $blue;
+                }
+            }
+            &-indicator {
+                position: absolute;
+                height: 3px;
+                background: $blue;
+                left: 0;
+                bottom: -1px;
+                width: 100px;
+                transition: all 250ms;
+            }
+        }
+        &-content {
+            padding: 8px 0;
+        }
+    }
+</style>
