c4823b00552318a6f1218c5492596337684c92d9
westee <904885508@qq.com>
2020-09-11 22:44:00 +0800

    首页渐变背景

--- vite-web/src/views/Home.vue
+++ vite-web/src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="home">
+    <div class="top-nav-banner">
         <Topnav></Topnav>
-        <div class="intro">
+        <div class="banner">
             <h1>Darkness UI</h1>
             <h2>基于Vue3 和 TypeScript 的 UI 框架</h2>
-            <div class="links">
+            <div class="actions">
                 <router-link to="/doc">查看文档</router-link>
                 <a href="github.com">GitHub</a>
             </div>
@@ -22,19 +22,32 @@
 </script>
 
 <style lang="scss" scoped>
-    .home {
+    .top-nav-banner {
+        background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+        clip-path: ellipse(80% 60% at 50% 40%);
+    }
+
+    .banner {
+        padding: 100px 0;
         display: flex;
+        justify-content: center;
+        align-items: center;
         flex-direction: column;
-        height: 100vh;
 
-        .intro {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            flex-direction: column;
-            padding: 100px 0;
+        > .actions {
+            padding: 8px 0;
+
+            a {
+                margin: 0 8px;
+                background: #fff;
+                display: inline-block;
+                $h: 28px;
+                height: $h;
+                line-height: $h;
+                border-radius: $h/2;
+                padding: 0 8px;
+            }
         }
-
     }
 
 </style>
