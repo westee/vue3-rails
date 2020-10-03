181d5a322e434c465e68b7f6e645d266d0993a68
westee <904885508@qq.com>
2020-10-03 22:44:03 +0800

    重构和markdown相关的页面

---
+++ src/components/Markdown.vue
@@ -0,0 +1,23 @@
+<template>
+    <article class="markdown-body" v-html="x"></article>
+</template>
+
+<script>
+    import {ref} from 'vue'
+    export default {
+
+        props:{
+          path: {
+              type: String
+          }
+        },
+        setup(props){
+            const x = ref();
+            console.log(props.path)
+            import(props.path).then((res)=>{
+                x.value = res.default
+            });
+            return {x}
+        }
+    }
+</script>
