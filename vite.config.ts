036be4b7fc6d1ebcf814080f0f1a1ea7a7013285
westee <904885508@qq.com>
2020-10-03 23:33:29 +0800

    展示源代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 // @ts-nocheck
-
-import { md } from "./plugins/md";
+import {md} from "./plugins/md";
 import fs from 'fs'
 import {baseParse} from '@vue/compiler-core'
 
@@ -8,17 +7,17 @@ export default {
     plugins: [md()],
     vueCustomBlockTransforms: {
         demo: (options) => {
-            const { code, path } = options
+            const {code, path} = options
             const file = fs.readFileSync(path).toString()
             const parsed = baseParse(file).children.find(n => n.tag === 'demo')
             const title = parsed.children[0].content
             const main = file.split(parsed.loc.source).join('').trim()
             return `export default function (Component) {
-        Component.__sourceCode = ${
-                JSON.stringify(main)
-            }
-        Component.__sourceCodeTitle = ${JSON.stringify(title)}
-      }`.trim()
+                Component.__sourceCode = ${
+                        JSON.stringify(main)
+                    }
+                Component.__sourceCodeTitle = ${JSON.stringify(title)}
+            }`.trim()
         }
     }
 };
