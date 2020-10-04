910b44ab184d69f9c415e6cfa4ed6d1d1c400a05
westee <904885508@qq.com>
2020-10-04 10:31:47 +0800

    更改doc默认路由

--- src/router.ts
+++ src/router.ts
@@ -23,7 +23,7 @@ export const router = createRouter({
         children: [
             {
                 path: '',
-                component: DocDemo
+                redirect: '/doc/intro'
             },{
                 path: 'intro',
                 component: md('intro')
